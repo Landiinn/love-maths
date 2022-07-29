9c4ccaf6ffd95e4477f2f8f9a029943e495cb193
Landiinn <andre.landin17@gmail.com>
2022-07-29 12:15:40 +0000

    adding core to calculate answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,6 +47,25 @@ function calculateCorrectAnswer() {
 
 }
 
+/**
+ * Gets the operands (the numbers) and he operator (plus, minus etc)
+ * directly from the dom
+ */
+function calculateCorrectAnswer() {
+
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}.Aborting!`;
+
+    }
+}
+
 function incrementScore() {
 
 }
